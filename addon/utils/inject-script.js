368bdc99bddaed58556280bf950655e1c092c87a
Ben Evans <benjamin.evans@digitaria.com>
2016-04-07 13:09:52 -0600

    Added ability to append the script to an element other than head.

--- addon/utils/inject-script.js
+++ addon/utils/inject-script.js
@@ -1,12 +1,21 @@
 import Ember from 'ember';
 
-export default function injectScript(src) {
+export default function injectScript(src, target = 'head') {
   return new Ember.RSVP.Promise(function(resolve) {
-    var script    = document.createElement('script');
+    let element;
+    let script    = document.createElement('script');
     script.type   = 'text/javascript';
     script.async  = true;
     script.src    = src;
-    script.onload = function() { resolve(); };
-    document.getElementsByTagName('head')[0].appendChild(script);
+    script.onload = function() {
+      resolve();
+    };
+    if (Ember.isPresent(target) && target !== 'head') {
+      element = document.getElementById(target);
+    }
+    if (Ember.isBlank(element)) {
+      element = document.getElementsByTagName('head')[0];
+    }
+    element.appendChild(script);
   });
 }
